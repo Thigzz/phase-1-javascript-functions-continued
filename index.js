cde998ecbb4dbd1f4385e7ccd91d220e8210523b
Ike Mwithiga <mwithigaike@gmail.com>
2025-03-20 10:58:38 +0000

    index file edits

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity) {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`;
+    } else {
+        return "This Saturday, I want to roller-skate!";
+    }
+}
+
+const mondayWork = function(action) {
+    if (action) {
+        return `This Monday, I will ${action}.`;
+    } else {
+        return "This Monday, I will go to the office."
+    }
+}
+
+function wrapAdjective(flair = "*") {
+    return function(trait = "special") {
+        return `You are ${flair}${trait}${flair}!`
+    }
+}
